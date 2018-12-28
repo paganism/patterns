14c7584431a78bedea67a8bd8c6334d342be14d0
YuriyB <paganism@mail.ru>
2018-12-28 23:22:40 +0300

    added singleton pattern

---
+++ singleton.py
@@ -0,0 +1,21 @@
+"""
+Singleton (Одиночка) - это паттерн, который порождает объекты.
+Одиночка гарантирует, что класс имеет только один инстанс (экземпляр) и предоставляет к нему доступ.
+С помощью этого паттерна можно реализовать паттерны, такие как абстрактная фабрика, строитель, прототип.
+"""
+
+class Singleton:
+    __instance = None
+
+    @staticmethod
+    def inst():
+        if Singleton.__instance == None:
+            Singleton.__instance = Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        print("Constructor called!")
+
+a = Singleton.inst()
+b = Singleton.inst()
+print('a is {}, \nb is {}, \na is b == {}'.format(a, b, (a is b)))
